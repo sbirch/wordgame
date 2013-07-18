cd541774fd3a019a6a5f4aaa0394909b48267dbe
Sam Birch <sam.m.birch@gmail.com>
2013-07-17 23:51:22 -0700

    game bits working

--- web.js
+++ web.js
@@ -9,18 +9,34 @@ app.use(express.static(__dirname + '/static'));
 function WordGame(){
 	this.p1 = null;
 	this.p2 = null;
+
+	this.letters = 'asdfghk';
 }
 
-WordGame.prototype.join = function(){
-	
+WordGame.prototype.join = function(socket){
+	if (this.p1 === null){
+		this.p1 = socket;
+		return 1;
+	}else if (this.p2 === null){
+		this.p2 = socket;
+		return 2;
+	}
+	return -1;
 }
 
+
+var games = {};
 
 
 io.sockets.on('connection', function(socket){
 	console.log('connected!')
 
-	socket.emit('handshake', 'Hello, world!')
+	socket.on('join', function(room, fn){
+		if (!games.hasOwnProperty(room)){
+			games[room] = new WordGame();
+		}
+		fn(games[room].join(socket));
+	})
     
     socket.on('disconnect', function(){
         console.log('disconnect!', arguments);
